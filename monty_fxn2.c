503327969e41ac1b781e6d34013846f00526dcb6
Jacob Biehler <biehlerj@gmail.com>
2017-12-22 23:28:13 +0000

    Implementing nop code

--- monty_fxn2.c
+++ monty_fxn2.c
@@ -20,17 +20,3 @@ void _add(stack_t **stack, unsigned int line_number)
 	jay->head->next->n = sum;
 	_pop(NULL, 0);
 }
-
-/**
- * _nop - does nothing
- * @stack: dummy variable
- * @line_number: dummy variable
- * Description: Does nothing to the stack
- * Return: Nothing (void)
- */
-void _nop(stack_t **stack, unsigned int line_number)
-{
-	(void) stack;
-	(void) line_number;
-	return;
-}
---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * _nop - does nothing
+ * @stack: dummy variable
+ * @line_number: dummy variable
+ * Description: Does nothing to the stack
+ * Return: Nothing (void)
+ */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
